bcf1b47053dfb6f87277987a7b342f0000c33879
Siddalingesha D S <siddhudev813@gmail.com>
2017-01-22 18:29:35 -0800

    Added another tag App2

---
+++ src/App2.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App2 extends Component{
+    render(){
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Learn React</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import App2 from './App2';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <App2 />
+  </div>,
   document.getElementById('root')
 );
